fc54334375afb6be4bbb084e752be0e4efde5583
Alexey Ospishchev <alexeyosp@gmail.com>
2022-02-10 22:22:21 +0300

    создали вью таблицу категорий

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CategoryController(AppDbContext db)
+        {
+            _db = db;   
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> catgories = _db.Category;
+;
+            return View(catgories);
+        }
+    }
+}
