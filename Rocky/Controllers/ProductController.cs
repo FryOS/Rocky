94a9f47e2ae8703fdf362a32aa7dfb157ac872e5
Alexey Ospishchev <alexeyosp@gmail.com>
2022-02-15 23:13:05 +0300

    Product

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _db.Product;
+
+            foreach (var product in products)
+            {
+                  product.Category = _db.Category.FirstOrDefault(u => u.Id == product.CategoryId);
+            };
+            
+            return View(products);
+        }
+
+
+        //Get - Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post - Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category category)
+        {
+            if (ModelState.IsValid) // валидация на стороне сервера
+            {
+                _db.Category.Add(category);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+
+        //Get - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Category.Find(id);
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        //Post - Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category category)
+        {
+            if (ModelState.IsValid) // валидация на стороне сервера
+            {
+                _db.Category.Update(category);
+                _db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            return View(category);
+
+        }
+        //delete
+
+        //Get - DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var category = _db.Category.Find(id);
+            if (category == null)
+                return NotFound();
+
+            return View(category);
+        }
+
+        //Post - Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var category = _db.Category.Find(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+                _db.Category.Remove(category);
+                _db.SaveChanges();
+                return RedirectToAction("Index");    
+        }
+
+
+    }
+}
