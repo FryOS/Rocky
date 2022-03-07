af17bc13163c1140a87fc47396f0a91a3443e805
Alexey Ospishchev <alexeyosp@gmail.com>
2022-03-07 23:23:24 +0300

    Контроллер корзина покупок

---
+++ Rocky/Controllers/CartController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Utility;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Rocky.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public CartController(AppDbContext db)
+        {
+            db = _db;
+        }
+
+        public IActionResult Index()
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart)!= null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                //сессия существует
+
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            List<int> prodInCart = shoppingCartList.Select(i => i.ProductId).ToList();
+            IEnumerable<Product> prodList = _db.Product.Where(u => prodInCart.Contains(u.Id));
+
+            return View(prodList);
+        }
+    }
+}
